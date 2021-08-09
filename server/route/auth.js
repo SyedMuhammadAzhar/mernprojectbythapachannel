e3e68e049b9889dece5b9083aa131fc6b0869cf8
Syed Muhammad Azhar <syed.moh.azhar.98@gmail.com>
2021-08-09 15:42:58 +0500

    Save user data to mongodb with async/await

--- server/route/auth.js
+++ server/route/auth.js
@@ -8,42 +8,80 @@ const router = express.Router();
 const User = require('../models/userSchema');
 
 
-router.post('/registration',(req,res)=>{
+router.post('/registration',async(req,res)=>{
 
     console.log("hello from registration");
 
     const {name,email,phone, work,password,cpassword,date}=req.body;
     
 
+
+
+
+    //With Async/Await
+
+    if(!name || !email || !phone || !work || !password || !cpassword,!date){
+
+             return res.json({message:"Fill all the field"});
+        
+          }
+
+          try{
+
+            const userExits=await User.findOne({email:email});
+
+            if(userExits){
+                return res.json({message:"User/email Already Exists"})
+            }
+            const newUser = new User({name,email,phone, work,password,cpassword,date});
+
+            await newUser.save();
+
+            res.json({message:"new user added"});
+          
+
+          }catch(err){
+              res.json({"error":"failed to register user",message:err});
+
+          }
+
+          
+          
+
+
+
+
+    //With Promises
+
     
-if(!name || !email || !phone || !work || !password || !cpassword,!date){
+// if(!name || !email || !phone || !work || !password || !cpassword,!date){
 
-    return res.json({message:"Fill all the field"});
+//     return res.json({message:"Fill all the field"});
 
-}
+// }
 
-    User.findOne({email:email}).then((emailExist)=>{
+//     User.findOne({email:email}).then((emailExist)=>{
 
-        if(emailExist){
-            return res.json({message:"Email already exist"});
-        }
-        else{
+//         if(emailExist){
+//             return res.json({message:"Email already exist"});
+//         }
+//         else{
 
-            const user = new User({name,email,phone, work,password,cpassword,date});
-            user.save().then(()=>{
-               return res.json({message:"New user added"});
-            })
-            .catch(err=>{
-                return res.json({error:"failed to register user",
-                                message:err
-                                });
-            })
+//             const user = new User({name,email,phone, work,password,cpassword,date});
+//             user.save().then(()=>{
+//                return res.json({message:"New user added"});
+//             })
+//             .catch(err=>{
+//                 return res.json({error:"failed to register user",
+//                                 message:err
+//                                 });
+//             })
 
-        }
+//         }
 
 
-    })
-    .catch(err=> res.json({messgae:err}))
+//     })
+//     .catch(err=> res.json({messgae:err}))
 
 
 
